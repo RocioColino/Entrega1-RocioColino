eca5d2d961080fcfa9750c8aa98ac327381bc39c
RocioColino <rcolino.tr@gmail.com>
2022-07-06 09:53:49 +0900

    Probando de nuevo

--- Entrega1/Entrega1/asgi.py
+++ Entrega1/Entrega1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ProyectoCoder project.
+ASGI config for Entrega1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoCoder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Entrega1.settings')
 
 application = get_asgi_application()
--- Entrega1/Entrega1/wsgi.py
+++ Entrega1/Entrega1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ProyectoCoder project.
+WSGI config for Entrega1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoCoder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Entrega1.settings')
 
 application = get_wsgi_application()
